486904aec278ff296a77c45251b68ba1c19a94eb
Hanna Song <hannaesong@localhost.localdomain>
2021-09-28 17:15:31 +0200

    Added blog descriription

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.TextField()
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='blog/images/', blank=True)
 
     def __str__(self):
